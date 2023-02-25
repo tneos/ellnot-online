52d67613488159e9f8c5b7da07e1dcdf4ee0f717
Thomas Neos <tneos@hotmail.com>
2023-02-25 13:08:36 +0000

    Link front and back end

--- backend-api/app.js
+++ backend-api/app.js
@@ -46,9 +46,6 @@ console.log(process.env.MODE);
 // Load React App in production
 
 if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  //app.use(express.static("../client/build"));
-
   app.get("/*", (req, res) => res.sendFile(path.join(__dirname, "../client/build/index.html")));
 }
 
