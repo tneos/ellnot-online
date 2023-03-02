5f602da9cc30fa09a8918f42508359fafee679b7
Thomas Neos <tneos@hotmail.com>
2023-03-02 19:32:02 +0000

    Checking search bar

--- client/src/utils/filterOutput.js
+++ client/src/utils/filterOutput.js
@@ -3,14 +3,15 @@ export const filterOutput = (output, data) => {
   let splittedOutput;
   let keysArrays = [];
 
-  output.map(array => {
-    Array.isArray(array) &&
-      Array.isArray(data) &&
-      array.length === data.length &&
-      array.every((val, index) => {
-        if (val === data[index]) return array;
-      });
-  });
+  output.length !== 0 &&
+    output.map(array => {
+      Array.isArray(array) &&
+        Array.isArray(data) &&
+        array.length === data.length &&
+        array.every((val, index) => {
+          if (val === data[index]) return array;
+        });
+    });
   console.log(output, data);
 
   data.map(arr => {
@@ -20,15 +21,15 @@ export const filterOutput = (output, data) => {
   uniq = [...new Set(output)];
   console.log(uniq);
 
-  //if (typeof uniq === "object") return uniq;
-  if (uniq.every(el => typeof el === "string")) return uniq;
+  if (typeof uniq === "object") return uniq;
+  //if (uniq.every(el => typeof el === "string")) return uniq;
   else {
-    // uniq.map(item => {
-    //   console.log(item);
-    //   typeof item === "string"
-    //     ? (splittedOutput = item.toLowerCase().split(" "))
-    //     : (splittedOutput = item.description.toLowerCase().split(" "));
-    // });
+    uniq.map(item => {
+      console.log(item);
+      typeof item === "string"
+        ? (splittedOutput = item.toLowerCase().split(" "))
+        : (splittedOutput = item.description.toLowerCase().split(" "));
+    });
 
     //console.log(splittedOutput);
 
