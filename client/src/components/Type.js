d71e41dc689aa5f01e9ca7580d4b96f342b605e7
Thomas Neos <tneos@hotmail.com>
2023-06-27 17:53:19 +0100

    Add message when data loading

--- client/src/components/Type.js
+++ client/src/components/Type.js
@@ -112,7 +112,7 @@ const Type = () => {
     output = (
       <Page title={capitalizeFirstLetter(type)}>
         <section className="items">
-          <h3 className="items__title">
+          <h3 className="items__notification">
             Please allow some time for data to load as we're using server's free tier. Thank you
           </h3>
         </section>
