98afc425af763d28747b2ae5763b84e9460ff329
Thomas Neos <tneos@hotmail.com>
2024-02-06 18:30:28 +0000

    Configure vercel.json

--- backend-api/config/db.js
+++ backend-api/config/db.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
-// const config = require("config");
-// const db = config.get("mongoURI");
+const config = require("config");
+const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
     mongoose.set("strictQuery", false);
-    mongoose.connect(process.env.MONGO_URI, {
+    mongoose.connect(db, {
       useNewUrlParser: true,
     });
 
