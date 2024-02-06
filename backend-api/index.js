1ac21c1fe1e02231095f5aa627bf48418779a2b0
Thomas Neos <tneos@hotmail.com>
2024-02-06 18:00:05 +0000

    Configure vercel.json

--- backend-api/index.js
+++ backend-api/index.js
@@ -49,6 +49,16 @@ connectDB();
 //   app.get("/*", (req, res) => res.sendFile(path.join(__dirname, "../client/build/index.html")));
 // }
 
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
 });
